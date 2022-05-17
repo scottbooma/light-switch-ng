a74faea9dcd63935026d92b783ce908fa5f903a2
scottbooma <scottbooma@gmail.com>
2022-05-17 13:48:56 -0400

    Create components

---
+++ src/app/night-light/night-light.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-night-light',
+  templateUrl: './night-light.component.html',
+  styleUrls: ['./night-light.component.css']
+})
+export class NightLightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
